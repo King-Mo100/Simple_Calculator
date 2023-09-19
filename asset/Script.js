2557a9924568671212602be9e63028f73393ddcb
moses <mking78911@gmail.com>
2023-09-19 19:23:19 +0100

    operator function

--- asset/Script.js
+++ asset/Script.js
@@ -4,3 +4,11 @@ function clearDisplay() {
     preValue = 0;
     display.innerText = '0';
 }
+
+function setOperator(operator) {
+    if (currentInput !== '') {
+        currentOperator = operator;
+        preValue = parseFloat(currentInput);
+        currentInput = '';
+    }
+}
